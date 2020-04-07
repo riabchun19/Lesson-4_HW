1025adaf36308174c80b5d98ea1096bcb0654459
AlexRiabchun <olexandrriab364@gmail.com>
2020-04-07 22:35:14 +0300

    starting

---
+++ HW-3/OOP_4_TEST/Program.cs
@@ -0,0 +1,31 @@
+﻿using System; 
+using System.Collections.Generic;
+using System.Linq; 
+
+namespace LambdaExpressions
+{ 
+    class Program
+    {   // 3. *Дан фрагмент программы:
+        // а) Свернуть обращение к OrderBy с использованием лямбда-выражения$ 
+        // б) *Развернуть обращение к OrderBy с использованием делегата Func<KeyValuePair<string, int>, int>.
+
+        static void Main()
+        {
+            Dictionary<string, int> dict = new Dictionary<string, int>()
+            {
+            {"four",4 },
+            {"two",2 },
+            { "one",1 },
+            {"three",3 },
+            };
+
+            var d = dict.OrderBy(delegate (KeyValuePair<string, int> pair) { return pair.Value; });
+
+            foreach (var pair in d)
+            {
+                Console.WriteLine("{0} - {1}", pair.Key, pair.Value);
+            }
+            Console.ReadKey();
+        }
+    } 
+}
